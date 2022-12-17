23a04e9eafe1eda479b6625bc57c53d4725e7ee7
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-17 11:51:54 +0530

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Ashish Sharma on 12/17/2022.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
